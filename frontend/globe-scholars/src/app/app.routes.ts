1bfff913c5f87c728d49e4eb99e997340d209d73
Rhoda Ikhuohon <rhodaivioje@gmail.com>
2026-02-15 12:37:36 +0100

    feat: implement routing and update app structure

--- frontend/globe-scholars/src/app/app.routes.ts
+++ frontend/globe-scholars/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './external/landing-page/landing-page';
+import { Register } from './external/register/register';
+import { Login } from './external/login/login';
+import { AboutComponent } from './external/about-component/about-component';
+import { ScholarsComponent } from './internal/scholars-component/scholars-component';
+import { RepositoryComponent } from './internal/repository-component/repository-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPage
+    },
+    {
+        path: 'register',
+        component: Register
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'scholars',
+        component: ScholarsComponent
+    },
+    {
+        path: 'repository',
+        component: RepositoryComponent,
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
---
+++ frontend/globe-scholars/src/app/internal/scholars-component/scholars-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scholars-component',
+  imports: [],
+  templateUrl: './scholars-component.html',
+  styleUrl: './scholars-component.scss',
+})
+export class ScholarsComponent {
+
+}
